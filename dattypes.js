d98a66637c38936e75a0ce316bb28157eb6f2257
IDAHOBOBO <osemwegiebigi@gmail.com>
2026-08-31 15:40:35 +0100

    Add files via upload

---
+++ dattypes.js
@@ -0,0 +1,12 @@
+let trashCan = null
+
+
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof trashCan)
+console.log(typeof isMarried)
+console.log(wantsToGoPro)
+console.log(myNum)
+consolelog(typeof likesAnyBody)
---
+++ variable.js
@@ -0,0 +1,29 @@
+let name = 'Idahosa Peter'
+//let age = 22
+let likesAnyBody = undefined
+let job = null
+const isMarried = false
+let wantsToGoPro = true
+
+let myNum = -4.56
+
+let ageGroup
+let neverBrokeAgain
+let tomaHawk
+let windMill
+
+
+let weather = 'fair'
+let seasons = 'Winter, Autumn, Spring, Summer'
+let nigerianWeather = 'Rainy, Harmatan'
+
+let firstName = 'Lorde'
+let lastName = 'Landre'
+let marritalStatus = 'NO'
+let country = 'United States Of America'
+let age = 22
+
+let newFrstName = 'NBA'; let newLastName = 'Player'; let newMaritalStatus = 'Yes'; let newCountry = 'Spain'; let newAge = 45
+
+let myAge = 'I am 25 years old'
+let yourAge = 'You are 30 years old' 
