abfd7b7a3616e759a1f0b2e1d055f3f106594f71
IDAHOBOBO <osemwegiebigi@gmail.com>
2026-09-07 10:52:07 +0100

    Add files via upload

---
+++ datatypesday2.js
@@ -0,0 +1,23 @@
+/*let trashCan = null
+
+
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof trashCan)
+console.log(typeof isMarried)
+console.log(wantsToGoPro)
+console.log(myNum)
+consolelog(typeof likesAnyBody)
+*/
+
+/**const num = Math.floor(Math.random() * 7)
+
+let fosaTechDevStudents = ['Jeffrey','Wizdom','Peter']
+
+fosaTechDevStudents[1] = 'James'
+
+let challenge = '30 Days of JavaScript'
+console.log(challenge)*/
+ 
---
+++ variableday2.js
@@ -0,0 +1,185 @@
+/*let name = 'Idahosa Peter'
+//let age = 22
+let likesAnyBody = undefined
+let job = null
+const isMarried = false
+let wantsToGoPro = true
+
+let myNum = -4.56
+
+let ageGroup
+let neverBrokeAgain
+let tomaHawk
+let windMill
+
+
+let weather = 'fair'
+let seasons = 'Winter, Autumn, Spring, Summer'
+let nigerianWeather = 'Rainy, Harmatan'
+
+let firstName = 'Lorde'
+let lastName = 'Landre'
+let marritalStatus = 'NO'
+let country = 'United States Of America'
+let age = 22
+
+let newFrstName = 'NBA'; let newLastName = 'Player'; let newMaritalStatus = 'Yes'; let newCountry = 'Spain'; let newAge = 45
+
+let myAge = 'I am 25 years old'
+let yourAge = 'You are 30 years old'  */
+
+
+//This is Day 2 Assignment
+
+//
+console.log('In every programming language it starts with\"hello World!\"')
+
+let challenge = '30 Days of JavaScipt'
+
+
+
+console.log(challenge)
+
+challenge.length
+
+challenge.toUpperCase
+
+
+challenge.toUpperCase()
+
+challenge.toLowerCase()
+
+challenge = '30 Days Of JavaScript'
+
+challenge.substr(3,18)
+
+challenge.substring(3,21)
+
+challenge.substr(0,3)
+
+challenge.includes('Script')
+
+challenge.split(' ')
+
+challenge.split('')
+
+challenge.split()
+
+
+console.log(company)
+
+let company = 'Facebook, Google, Amazon, Microsoft, IBM, Apple, Oracle'
+
+company.split(', ')
+
+challenge.replace('javaScript', 'Python')
+
+
+challenge.indexOf('S')
+
+challenge.charCodeAt('j')
+
+challenge.indexOf('a')
+
+challenge.lastIndexOf('a')
+
+let position = 'You cannot end a sentence with because because because is a conjunction'
+
+position.lastIndexOf('because')
+
+position.indexOf('because')
+
+position.search('because')
+
+challenge.trim()
+
+challenge.startsWith('30')
+
+challenge.endsWith('Script')
+
+challenge.match('a')
+
+console.log(challenge.charAt(15))
+
+
+console.log(challenge.charCodeAt(12))
+
+
+console.log(challenge.indexOf(5))
+
+
+console.log(challenge.indexOf('a'))
+
+
+console.log(challenge.lastIndexOf('a'))
+
+let occur = 'You cannot end a sentence with because because because is a conjunction'
+
+console.log(occur.indexOf('because'))
+
+console.log(occur.lastIndexOf('because'))
+
+console.log(occur.search('because'))
+
+console.log(challenge.startsWith('30'))
+
+console.log(challenge.endsWith('ipt'))
+
+console.log(challenge.repeat(3))
+
+console.log(typeof '10')
+
+let num = '12'
+
+let newNum = parseInt(num)
+
+console.log(newNum)
+
+
+console.log(typeof num)
+
+console.log(typeof newNum)
+
+
+let numFlo = '30.95'
+
+let newFlo = Number(numFlo)
+
+console.log(typeof newFlo)
+
+
+let brk = '1 1 1 1 1\n 2 1 2 4 8\n 3 1 3 9 27\n 4 1 4 16 64\n 5 1 5 25 125'
+
+console.log(brk)
+
+let myn = Math.random()
+
+let newMyn = myn * 100
+
+console.log(newMyn)
+
+console.log(occur.substr(32,54))
+
+console.log(occur.substr(32,23))
+
+let bet = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+
+console.log(bet.search('Love'))
+
+console.log(bet.search('love'))
+
+
+let ran = Math.floor(Math.random()* (100-50) + 50)
+undefined
+console.log(ran)
+
+console.log(ran)
+
+Math.random() * (300 - 120) + (120)
+
+Math.random() * (500 - 250) + (250)
+
+console.log(bet.match('because'))
+
+console.log(occur.match('because'))
+
